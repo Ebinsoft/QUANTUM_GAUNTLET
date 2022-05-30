c72dacd70ad925c8e8bc81e8afd1641efb651233
Tyler Thurston <tylerthurston1@gmail.com>
2022-05-30 18:07:15 -0400

    Better kinematic movement/jump, added gravity.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,13 +5,36 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float playerSpeed = 3.5f;
-    public float jumpForce = 2.0f;
-    private Rigidbody rb;
+    CharacterController characterController;
+
+    private Vector3 currentMovement;
     private PlayerInput playerInput;
+    private Rigidbody rb;
+    public float playerSpeed = 3.5f;
+
+    // jumping variables
+    public float jumpForce = 2.0f;
+    public float maxJumps = 2;
+    private float jumpsLeft;
+
+
+    // gravity variables
+    float gravity = -4.0f;
+    float groundedGravity = -0.05f;
 
     private void Awake() {
         playerInput = new PlayerInput();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void handleGravity() 
+    {
+        if(characterController.isGrounded) {
+            currentMovement.y = groundedGravity;
+        }
+        else {
+            currentMovement.y = gravity * Time.deltaTime;
+        }
     }
 
     private void OnEnable()
@@ -36,21 +57,32 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        jumpsLeft = maxJumps;
     }
 
     private void Jump(InputAction.CallbackContext context) {
-        rb.MovePosition(transform.position + new Vector3(0f, jumpForce, 0f));
+        if(characterController.isGrounded && jumpsLeft < maxJumps) {
+            jumpsLeft = maxJumps;
+        }
+
+        if(jumpsLeft > 0) {
+            characterController.Move(new Vector3(0.0f, jumpForce, 0.0f));
+            jumpsLeft -= 1;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentMovement = new Vector3(0.0f, 0.0f, 0.0f);
         if(playerInput.Player.Move.inProgress) {
             Vector2 move = playerInput.Player.Move.ReadValue<Vector2>();
-            Vector3 velocity = new Vector3(playerSpeed * Time.deltaTime * move.x, 0f, playerSpeed * Time.deltaTime * move.y);
-            transform.position += velocity;
+            currentMovement = new Vector3(playerSpeed * Time.deltaTime * move.x, 0.0f, playerSpeed * Time.deltaTime * move.y);
+            
         }
+        handleGravity();
+        characterController.Move(currentMovement);
     }
 
 }
