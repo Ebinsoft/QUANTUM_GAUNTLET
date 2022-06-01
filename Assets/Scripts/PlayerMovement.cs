12ee9eb2c943c350024d96b6f68d9e297c8b9c65
Tyler Thurston <tylerthurston1@gmail.com>
2022-05-31 20:04:40 -0400

    Change jump from instant to floaty.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,37 +5,50 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
-    CharacterController characterController;
-
-    private Vector3 currentMovement;
     private PlayerInput playerInput;
+    private CharacterController characterController;
     private Rigidbody rb;
+
+    // movement variables
+    private Vector3 currentMovement;
     public float playerSpeed = 3.5f;
 
     // jumping variables
-    public float jumpForce = 2.0f;
     public float maxJumps = 2;
-    [SerializeField]
+    public float maxJumpHeight = 1.0f;
+    public float maxJumpTime = 0.5f;
+    private float initialJumpVelocity;
+    private bool isJumpPressed = false;
     private float jumpsLeft;
-    private bool isJumpPressed;
-
 
     // gravity variables
-    float gravity = -4.0f;
-    float groundedGravity = -0.05f;
+    private float gravity;
+    private float groundedGravity = -0.05f;
 
     private void Awake() {
         playerInput = new PlayerInput();
         characterController = GetComponent<CharacterController>();
+        setupJumpVariables();
     }
+
+    void setupJumpVariables()
+    {
+        // setting gravity and our jump velocity in terms of jump height and jump time
+        float timeToApex = maxJumpTime / 2;
+        gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+    }
+
 
     void handleGravity() 
     {
+        // a lower grounded gravity makes clipping less likely but will still trigger isGrounded
         if(characterController.isGrounded) {
-            currentMovement.y += groundedGravity;
+            currentMovement.y = groundedGravity;
         }
         else {
             currentMovement.y += gravity * Time.deltaTime;
+            currentMovement.y = Mathf.Max(currentMovement.y, gravity);
         }
     }
 
@@ -45,6 +58,7 @@ public class PlayerMovement : MonoBehaviour
 
         // subscribe to events
         playerInput.Player.Jump.started += Jump;
+        playerInput.Player.Jump.canceled += Jump;
     }
 
     private void OnDisable()
@@ -53,6 +67,7 @@ public class PlayerMovement : MonoBehaviour
 
         // unsubscribe to events
         playerInput.Player.Jump.started -= Jump;
+        playerInput.Player.Jump.canceled -= Jump;
     }
 
     // Start is called before the first frame update
@@ -63,22 +78,19 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Jump(InputAction.CallbackContext context) {
-
-
-        if(jumpsLeft > 0) {
-            characterController.Move(new Vector3(0.0f, jumpForce, 0.0f));
-            jumpsLeft -= 1;
+        isJumpPressed = context.ReadValueAsButton();
+        if(jumpsLeft > 0 && isJumpPressed == true) {
+            currentMovement.y = initialJumpVelocity;
+            jumpsLeft--;
         }
-        
     }
 
-    // if we touch the ground, we want to gain all of our jumps back
-    // however, IIRC, in smash if you walk off an edge(or get knocked off)
-    // you'll lose your first jump, so this will reduce max jumps by one if not grounded
     private void resetJumps() {
         if(characterController.isGrounded && jumpsLeft < maxJumps) {
             jumpsLeft = maxJumps;
         }
+        // Steals a jump if you're not on the ground(like smash). This part is finicky since we're 
+        // relying on isGrounded. Should probably find a better way or remove the jump stealing.
         else if(!characterController.isGrounded && jumpsLeft == maxJumps) {
             jumpsLeft = maxJumps-1;
         }
@@ -87,19 +99,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // reset currentMovement each frame and just combine everything into it that runs in update
-        currentMovement = new Vector3(0.0f, 0.0f, 0.0f);
-
-        // reset jumps, if you fall 
+        currentMovement = new Vector3 (0.0f, currentMovement.y, 0.0f);
         resetJumps();
 
         if(playerInput.Player.Move.inProgress) {
             Vector2 move = playerInput.Player.Move.ReadValue<Vector2>();
-            currentMovement += new Vector3(playerSpeed * Time.deltaTime * move.x, 0.0f, playerSpeed * Time.deltaTime * move.y);
-            
+            currentMovement += new Vector3(playerSpeed * move.x, 0.0f, playerSpeed * move.y);
         }
+        characterController.Move(currentMovement * Time.deltaTime);
         handleGravity();
-        characterController.Move(currentMovement);
     }
 
 }
