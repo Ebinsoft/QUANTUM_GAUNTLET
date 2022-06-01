b8648bc57e647c875a799b42223f84eb4f80d221
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-01 19:08:51 -0400

    Restructure files and add PlayerManager

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Vector3 currentMovement;
+    private CharacterController characterController;
+
+    void Awake() {
+        characterController = GetComponent<CharacterController>();
+        currentMovement = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+}
