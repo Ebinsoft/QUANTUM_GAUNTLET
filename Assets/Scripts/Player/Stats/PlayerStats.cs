022852727a18ef4ab37ed27cc53c4fa3eb5413c9
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-13 14:03:09 -0400

    Add player stats currentStatus.

--- Assets/Scripts/Player/Stats/PlayerStats.cs
+++ Assets/Scripts/Player/Stats/PlayerStats.cs
@@ -6,4 +6,55 @@ public class PlayerStats : MonoBehaviour
 {
     private PlayerBaseState baseStats;
     public int health = 100;
+
+
+    private Status _currentStatus;
+    public bool canTakeDamage { get; private set; }
+    public bool canGetHit { get; private set; }
+    public bool canGetStunned { get; private set; }
+
+    public enum Status
+    {
+        normal,
+        intangible,
+        armored,
+        invulnerable
+    }
+
+    public Status currentStatus
+    {
+        get { return _currentStatus; }
+        set
+        {
+            switch (value)
+            {
+                case Status.normal:
+                    canTakeDamage = true;
+                    canGetHit = true;
+                    canGetStunned = true;
+                    break;
+                case Status.intangible:
+                    canTakeDamage = false;
+                    canGetHit = false;
+                    canGetStunned = false;
+                    break;
+                case Status.armored:
+                    canTakeDamage = true;
+                    canGetHit = true;
+                    canGetStunned = false;
+                    break;
+                case Status.invulnerable:
+                    canTakeDamage = false;
+                    canGetHit = true;
+                    canGetStunned = false;
+                    break;
+                default:
+                    Debug.LogError("Incorrectly set stat.Status");
+                    break;
+            }
+        }
+
+    }
+
+
 }
