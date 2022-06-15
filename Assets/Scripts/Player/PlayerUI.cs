df533d10ae596a5ef18e4d065ee017e7b9e80995
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-14 21:56:24 -0400

    Rename StateUI to PlayerUI and move script to Player folder.

--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StateUI : MonoBehaviour
+public class PlayerUI : MonoBehaviour
 {
     public Text stateText;
     public PlayerManager player;
