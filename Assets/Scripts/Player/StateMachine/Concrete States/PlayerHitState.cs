e5f2e782cdc2fc82f6f4dea3d77261735ddd2524
Jesse Holzman <jesseholzman96@gmail.com>
2022-06-19 11:45:10 -0400

    Reimplement verlet velocity with speed rather than time.

--- Assets/Scripts/Player/StateMachine/Concrete States/PlayerHitState.cs
+++ Assets/Scripts/Player/StateMachine/Concrete States/PlayerHitState.cs
@@ -8,18 +8,22 @@ public class PlayerHitState : PlayerBaseState
     private PlayerManager player;
     private HitData hitAttack;
 
+    // computed as the direction from attacker to me
     private Vector3 knockbackDirection;
 
+    // how far into the stuntime we begin decelerating
     private float percentAtFullSpeed = 0.5f;
 
     private float startTime;
 
+    // KNOCKBACK VARIABLES
     private float stunTime, knockbackDist;
     private float fullSpeedTime, decelTime;
     private float fullSpeedDist, decelDist;
     private float fullSpeed;
-    private float decelRate;
-    private float prevDecTime;
+
+    // tracks previous frame's speed for verlet calculation
+    private float prevFrameSpeed;
 
     public PlayerHitState(PlayerManager psm) : base(psm)
     {
@@ -29,6 +33,8 @@ public class PlayerHitState : PlayerBaseState
     public override void EnterState()
     {
         player.isHit = true;
+
+        // pull out hitData and reset the trigger
         hitAttack = player.triggerHit.Value;
         player.triggerHit = null;
 
@@ -40,8 +46,8 @@ public class PlayerHitState : PlayerBaseState
         // force the animator to ignore its current transition rules and play the stun animation
         player.anim.Play("Take Hit");
 
+        // begin stun timer
         startTime = Time.time;
-        prevDecTime = startTime + fullSpeedTime;
     }
 
     private void initiateStun()
@@ -57,11 +63,16 @@ public class PlayerHitState : PlayerBaseState
         decelDist = (decelTime / (2 * fullSpeedTime + decelTime)) * knockbackDist;
         fullSpeedDist = knockbackDist - decelDist;
 
+        // calculate initial knockback speed
         fullSpeed = fullSpeedDist / fullSpeedTime;
+
+        // reset verlet previous frame speed to full speed
+        prevFrameSpeed = fullSpeed;
 
         // calculate knockback direction
         knockbackDirection = (player.transform.position - hitAttack.origin.position).normalized;
 
+        // point player towards the attacker
         player.rotationTarget.x = -knockbackDirection.x;
         player.rotationTarget.y = -knockbackDirection.z;
     }
@@ -84,10 +95,15 @@ public class PlayerHitState : PlayerBaseState
                 // player.currentMovement.z = (1 - timeDecelerating / decelTime) * fullSpeed * knockbackDirection.z;
                 // prevDecTime = currentDecTime;
 
-                // deceleration calc without verlet
                 float timeDecelerating = Time.time - (startTime + fullSpeedTime);
-                player.currentMovement.x = (1 - timeDecelerating / decelTime) * fullSpeed * knockbackDirection.x;
-                player.currentMovement.z = (1 - timeDecelerating / decelTime) * fullSpeed * knockbackDirection.z;
+                float currentSpeed = Mathf.Clamp((1 - timeDecelerating / decelTime),0,1) * fullSpeed;
+
+                float verletSpeed = (currentSpeed + prevFrameSpeed) / 2;
+
+                player.currentMovement.x = verletSpeed * knockbackDirection.x;
+                player.currentMovement.z = verletSpeed * knockbackDirection.z;
+
+                prevFrameSpeed = currentSpeed;
             }
         }
     }
