9cb33814b324196d4fc3a10da16e3bbc7acb272f
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-19 11:19:11 -0400

    Make FPS counter readable.

--- Assets/Scripts/Player/FPSCounter.cs
+++ Assets/Scripts/Player/FPSCounter.cs
@@ -6,11 +6,26 @@ public class FPSCounter : MonoBehaviour
 {
     public Text fpsText;
     public float deltaTime;
+    private float fps = 0.0f;
+    private float FPS = 5;
 
+    void Start()
+    {
+        StartCoroutine(updateUI());
+    }
     void Update()
     {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-        float fps = 1.0f / deltaTime;
-        fpsText.text = Mathf.Ceil(fps).ToString();
+        fps = 1.0f / deltaTime;
+    }
+
+    IEnumerator updateUI()
+    {
+        while (true)
+        {
+            fpsText.text = Mathf.Ceil(fps).ToString();
+            yield return new WaitForSeconds(.2f);
+        }
+
     }
 }
