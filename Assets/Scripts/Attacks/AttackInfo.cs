21cee93f803ab9c1560149c54427f8642dc0f59b
Jesse Holzman <jesseholzman96@gmail.com>
2022-06-20 12:00:54 -0400

    Rework AttackInfo fields with lots of custom editor stuff.

--- Assets/Scripts/Attacks/AttackInfo.cs
+++ Assets/Scripts/Attacks/AttackInfo.cs
@@ -7,10 +7,57 @@ public class AttackInfo : ScriptableObject
     public string attackName;
     public int damage;
     public float hitlagTime;
-    public float stunTime;
-    public float knockback;
-    public float knockup;
-    public float knockupTime;
+
+    // KNOCKBACK
+    public float knockbackAngle;
+    public float knockbackMagnitude;
+    public Vector2 knockback
+    {
+        get
+        {
+            var vec = new Vector2();
+            vec.x = knockbackMagnitude * Mathf.Sin(Mathf.Deg2Rad * knockbackAngle);
+            vec.y = knockbackMagnitude * Mathf.Cos(Mathf.Deg2Rad * knockbackAngle);
+            return vec;
+        }
+    }
+
+    // STUN
+    public StunCalculation stunCalculation;
+    public float baseStun;
+    const float knockbackStunMultiplier = 4f / 60;
+    public float knockbackStun
+    {
+        get { return knockbackMagnitude * knockbackStunMultiplier; }
+    }
+    public float stunTime
+    {
+        get
+        {
+            switch (stunCalculation)
+            {
+                case StunCalculation.combined:
+                    return baseStun + knockbackStun;
+
+                case StunCalculation.baseOnly:
+                    return baseStun;
+
+                case StunCalculation.knockbackOnly:
+                    return knockbackStun;
+
+                default:
+                    Debug.LogError("Invalid StunCalculation type.");
+                    return 0;
+            }
+        }
+    }
+}
+
+public enum StunCalculation
+{
+    combined,
+    baseOnly,
+    knockbackOnly,
 }
 
 [CustomEditor(typeof(AttackInfo))]
@@ -31,20 +78,56 @@ public class AttackInfoEditor : Editor
         // Hitlag Time
         obj.hitlagTime = EditorGUILayout.FloatField("Hitlag Time", obj.hitlagTime);
         float hitlagFrames = obj.hitlagTime * 60;
+        EditorGUI.indentLevel++;
         EditorGUILayout.LabelField("(" + hitlagFrames + " frames of hitlag)");
+        EditorGUI.indentLevel--;
 
-        // Stun Time
-        obj.stunTime = EditorGUILayout.FloatField("Stun Time", obj.stunTime);
+        EditorGUILayout.Space();
 
         // Knockback
-        obj.knockback = EditorGUILayout.FloatField("Knockback Distance", obj.knockback);
+        EditorGUILayout.LabelField("Knockback", EditorStyles.boldLabel);
+        string angleArrow;
+        switch (obj.knockbackAngle)
+        {
+            case 0:
+                angleArrow = "↑";
+                break;
+            case 90:
+                angleArrow = "→";
+                break;
+            default:
+                angleArrow = "↗";
+                break;
+        }
+        obj.knockbackAngle = EditorGUILayout.Slider("Knockback Angle " + angleArrow, obj.knockbackAngle, 0, 90);
+        obj.knockbackMagnitude = EditorGUILayout.FloatField("Knockback Magnitude", obj.knockbackMagnitude);
+        using (new EditorGUI.DisabledScope(true))
+        {
+            // round displayed value to three decimal places
+            Vector2 roundedKnockback = new Vector2(
+                Mathf.Round(obj.knockback.x * 1000) / 1000,
+                Mathf.Round(obj.knockback.y * 1000) / 1000);
+            EditorGUILayout.Vector2Field("Knockback", roundedKnockback);
+        }
 
-        // Knockup
-        obj.knockup = EditorGUILayout.FloatField("Knockup Distance", obj.knockup);
+        EditorGUILayout.Space();
 
-        // Knockup Time
-        EditorGUI.BeginDisabledGroup(obj.knockup <= 0);
-        obj.knockupTime = EditorGUILayout.FloatField("Knockup Time", obj.knockupTime);
-        EditorGUI.EndDisabledGroup();
+        // Stun
+        EditorGUILayout.LabelField("Stun", EditorStyles.boldLabel);
+        obj.stunCalculation = (StunCalculation)EditorGUILayout.EnumPopup("Stun Calculation", obj.stunCalculation);
+
+        if (obj.stunCalculation != StunCalculation.knockbackOnly)
+        {
+            obj.baseStun = EditorGUILayout.FloatField("Base Stun", obj.baseStun);
+        }
+
+        using (new EditorGUI.DisabledScope(true))
+        {
+            if (obj.stunCalculation != StunCalculation.baseOnly)
+            {
+                EditorGUILayout.FloatField("Knockback Stun", obj.knockbackStun);
+            }
+            EditorGUILayout.FloatField("Total Stun", obj.stunTime);
+        }
     }
 }
