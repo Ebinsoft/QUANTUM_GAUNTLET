6a09fc3f6ffe3f8997e7fffea56ab85f4d0ff276
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-22 15:23:18 -0400

    Add CharacterData class to hold character prefabs.

---
+++ Assets/CharacterData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterData
+{
+    public string characterName;
+    public GameObject characterPrefab;
+}
