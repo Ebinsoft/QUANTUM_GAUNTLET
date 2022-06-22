d60cbb81445cac0075c496b2481c3e23fe9b4852
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-22 15:25:01 -0400

    Add SceneManager to spawn in characters.

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject player;
+    public PlayerInputManager playerInputManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterData c = GameManager.instance.roster.GetCharacter("Edmond");
+        player = c.characterPrefab;
+        GameManager.instance.numPlayers = 2;
+        for (int i = 0; i < GameManager.instance.numPlayers; i++)
+        {
+            SpawnCharacter(c);
+        }
+    }
+
+    void SpawnCharacter(CharacterData c)
+    {
+        playerInputManager.playerPrefab = c.characterPrefab;
+        playerInputManager.JoinPlayer();
+    }
+}
