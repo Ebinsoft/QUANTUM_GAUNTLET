1c0cf895a863bcc8a6c15ee046978eea11058e67
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-23 22:21:32 -0400

    VersusInfo.playerSettings now gets reset on start and will then populate per controller joined.

--- Assets/Scripts/Scenes/CharacterSelect/CharacterSelectManager.cs
+++ Assets/Scripts/Scenes/CharacterSelect/CharacterSelectManager.cs
@@ -10,13 +10,14 @@ public class CharacterSelectManager : MonoBehaviour
     {
         // reset versusInfo 
 
-        // versusInfo.numPlayers = 0;
-        // versusInfo.playerSettings = new List<PlayerSetting>();
+
     }
     // Start is called before the first frame update
     void Start()
     {
         versusInfo = GameManager.instance.versusInfo;
+        versusInfo.numPlayers = 0;
+        versusInfo.playerSettings = new List<PlayerSetting>();
     }
 
     // Update is called once per frame
@@ -27,11 +28,14 @@ public class CharacterSelectManager : MonoBehaviour
 
     void OnPlayerJoined(PlayerInput playerInput)
     {
+        versusInfo.numPlayers++;
         PlayerSetting ps = new PlayerSetting
         {
+            playerName = "Player " + playerInput.playerIndex + 1,
             playerIndex = playerInput.playerIndex,
             device = playerInput.devices[0],
             deviceString = playerInput.devices[0].ToString(),
+            playerType = "Human",
             team = "FFA"
         };
         versusInfo.playerSettings.Add(ps);
