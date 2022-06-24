3f86a7bea39252ba125c6e5adef341db75295ddb
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-23 22:03:02 -0400

    Copy GameManager over to new scene since it's just that easy.

--- Assets/Scripts/Scenes/CharacterSelect/CharacterSelectManager.cs
+++ Assets/Scripts/Scenes/CharacterSelect/CharacterSelectManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterSelectManager : MonoBehaviour
 {
+
+    private void Awake()
+    {
+        // reset versusInfo 
+        VersusInfo versusInfo = GameManager.instance.versusInfo;
+        // versusInfo.numPlayers = 0;
+        // versusInfo.playerSettings = new List<PlayerSetting>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnPlayerJoined()
+    {
+        Debug.Log("poggers");
     }
 }
