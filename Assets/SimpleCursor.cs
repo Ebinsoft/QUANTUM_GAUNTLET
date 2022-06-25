14f24a35cb3f24dbe093d9633b73250ded04d8a1
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-25 14:23:03 -0400

    Rewrite Character Select to be totally 3D and use proper raycasting.

--- Assets/SimpleCursor.cs
+++ Assets/SimpleCursor.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SimpleCursor : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class SimpleCursor : MonoBehaviour
     public float cursorSpeed = 1000f;
     private Vector2 currentMovement;
     private bool isMovePressed;
+    private PlayerSetting playerSetting;
+
+
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -16,7 +21,8 @@ public class SimpleCursor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // Look up this cursor's relevant playerSetting from the Game Manager
+        GetPlayerSetting();
     }
 
     private void onMove(InputAction.CallbackContext context)
@@ -28,6 +34,23 @@ public class SimpleCursor : MonoBehaviour
     private void onClick(InputAction.CallbackContext context)
     {
         Debug.Log("CLICK");
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit))
+        {
+            Debug.Log("Character Box Hit");
+            ChangeState(hit.collider.gameObject);
+        }
+    }
+
+    private void ChangeState(GameObject go)
+    {
+        CharacterBox characterBox = go.GetComponent<CharacterBox>();
+        if (characterBox != null)
+        {
+            // update our chosen character string
+            playerSetting.characterName = characterBox.GetCharacterName();
+        }
     }
 
     private void onStart(InputAction.CallbackContext context)
@@ -43,13 +66,29 @@ public class SimpleCursor : MonoBehaviour
         }
     }
 
+    private void GetPlayerSetting()
+    {
+        foreach (PlayerSetting ps in GameManager.instance.versusInfo.playerSettings)
+        {
+            if (ps.playerIndex == playerInput.playerIndex)
+            {
+                playerSetting = ps;
+                break;
+            }
+        }
+        if (playerSetting == null)
+        {
+            Debug.LogError("No playerSetting found with playerIndex of " + playerInput.playerIndex);
+        }
+    }
+
     void updateCursor()
     {
         currentMovement = playerInput.actions["Move"].ReadValue<Vector2>();
         Vector2 moveVector = new Vector2();
         moveVector.x = transform.position.x + currentMovement.x * cursorSpeed * Time.deltaTime;
         moveVector.y = transform.position.y + currentMovement.y * cursorSpeed * Time.deltaTime;
-        transform.position = moveVector;
+        transform.position = new Vector3(moveVector.x, moveVector.y, transform.position.z);
     }
 
     private void OnEnable()
@@ -59,7 +98,7 @@ public class SimpleCursor : MonoBehaviour
         playerInput.actions["Move"].canceled += onMove;
 
         playerInput.actions["Click"].started += onClick;
-        playerInput.actions["Click"].canceled += onClick;
+        // playerInput.actions["Click"].canceled += onClick;
 
         playerInput.actions["Start"].started += onStart;
         playerInput.actions["Start"].canceled += onStart;
@@ -72,7 +111,7 @@ public class SimpleCursor : MonoBehaviour
         playerInput.actions["Move"].canceled -= onMove;
 
         playerInput.actions["Click"].started -= onClick;
-        playerInput.actions["Click"].canceled -= onClick;
+        // playerInput.actions["Click"].canceled -= onClick;
 
         playerInput.actions["Start"].started -= onStart;
         playerInput.actions["Start"].canceled -= onStart;
