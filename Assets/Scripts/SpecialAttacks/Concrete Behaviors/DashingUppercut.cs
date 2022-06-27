3d1344ed0333bb0c467d2e3aea15d0cf441f21a8
Jesse Holzman <jesseholzman96@gmail.com>
2022-06-27 14:36:30 -0400

    Add logic for hitting and whiffing

--- Assets/Scripts/SpecialAttacks/Concrete Behaviors/DashingUppercut.cs
+++ Assets/Scripts/SpecialAttacks/Concrete Behaviors/DashingUppercut.cs
@@ -4,13 +4,59 @@ using UnityEngine;
 public class DashingUppercut : SpecialAttackBehavior
 {
 
-    public override void OnEnter() { }
+    private float timeBeforeWhiff = 0.5f;
 
-    public override void Update() { }
+    private float timer;
+    private bool firstHitTriggered;
+
+
+    private enum Phase
+    {
+        Searching,
+        Whiff,
+        Hit
+    }
+    private Phase currentPhase;
+
+    public override void OnEnter()
+    {
+        timer = 0;
+        firstHitTriggered = false;
+        currentPhase = Phase.Searching;
+    }
+
+    public override void Update()
+    {
+        timer += Time.deltaTime;
+
+        switch (currentPhase)
+        {
+            case Phase.Searching:
+                if (timer >= timeBeforeWhiff)
+                {
+                    currentPhase = Phase.Whiff;
+                    player.anim.SetTrigger("UppercutWhiff");
+                }
+                break;
+
+            case Phase.Whiff:
+                break;
+
+            case Phase.Hit:
+                break;
+        }
+    }
 
     public override void OnExit() { }
 
-    public override void OnHit(Collider other) { }
+    public override void OnHit(Collider other)
+    {
+        if (currentPhase == Phase.Searching)
+        {
+            currentPhase = Phase.Hit;
+            player.anim.SetTrigger("UppercutHit");
+        }
+    }
 
     public override void TriggerAction(int actionID)
     {
