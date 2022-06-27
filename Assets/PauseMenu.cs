4ff4f95de23686ab63e974e6b8ca2d0de21dde8b
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-27 14:30:41 -0400

    Add GameOver menu and add debug button to toggle it until it is properly triggering with game end logic.

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour
+{
+    private GameObject pauseMenuPane;
+    private GameObject firstSelectedButton;
+    private bool isEnabled = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuPane = transform.Find("Pane").gameObject;
+        firstSelectedButton = pauseMenuPane.transform.Find("Buttons").Find("Restart").gameObject;
+        isEnabled = false;
+        DisableGameOver();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToggleMenu()
+    {
+        if (!isEnabled)
+        {
+            EnableGameOver();
+        }
+        else
+        {
+            DisableGameOver();
+        }
+        isEnabled = !isEnabled;
+    }
+
+    public void EnableGameOver()
+    {
+        Time.timeScale = 0f;
+        pauseMenuPane.SetActive(true);
+
+        // clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        // set starting button
+        EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+    }
+
+    public void DisableGameOver()
+    {
+        Time.timeScale = 1f;
+        pauseMenuPane.SetActive(false);
+    }
+}
