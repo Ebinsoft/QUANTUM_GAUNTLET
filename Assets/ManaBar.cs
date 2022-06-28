b151e17f4ef0d097004d1afc829e46acd5239ca8
Tyler Thurston <tylerthurston1@gmail.com>
2022-06-27 22:23:56 -0400

    Add extremely ugly but functional health and mana bars.

---
+++ Assets/ManaBar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    public Slider slider;
+    public PlayerManager player;
+    public Gradient manaGradient;
+    private Image fill;
+
+    private void Start()
+    {
+        slider.maxValue = player.stats.baseStats.baseMana;
+        fill = transform.Find("Fill").GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        UpdateHealthBar();
+
+    }
+
+    public void UpdateHealthBar()
+    {
+        slider.value = Mathf.Max(0, player.stats.mana);
+        fill.color = manaGradient.Evaluate(slider.normalizedValue);
+    }
+}
