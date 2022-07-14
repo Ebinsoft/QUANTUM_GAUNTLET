60ad74b2ee60376e0e70f9e8116bb0959050dd52
Jordan Reef <jreef3@gatech.edu>
2022-07-14 18:23:41 -0400

    Update LevelToon_v2_shader graph. Add post-processing stack outline shader. Update scene Main Camera with components to support.

---
+++ Assets/Shaders/EnviornmentOutline/PostProcessOutline.cs
@@ -0,0 +1,41 @@
+﻿// Adapted from a shader tutorial from 
+// https://roystan.net/articles/outline-shader.html
+
+using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(PostProcessOutlineRenderer), PostProcessEvent.BeforeStack, "Custom/Post Process Outline")]
+public sealed class PostProcessOutline : PostProcessEffectSettings
+{
+    public ColorParameter color = new ColorParameter { value = Color.white };
+    public IntParameter scale = new IntParameter { value = 1 };
+    public FloatParameter depthThreshold = new FloatParameter { value = 0.2f };
+    public FloatParameter normalThreshold = new FloatParameter { value = 0.4f };
+    
+    [Range(0,1)]
+    public FloatParameter depthNormalThreshold = new FloatParameter { value = 0.5f };
+    public FloatParameter depthNormalThresholdScale = new FloatParameter { value = 7 };
+}
+
+public sealed class PostProcessOutlineRenderer : PostProcessEffectRenderer<PostProcessOutline>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Custom/Outline Post Process"));
+
+        sheet.properties.SetColor("_Color", settings.color);
+        sheet.properties.SetFloat("_Scale", settings.scale);
+        sheet.properties.SetFloat("_DepthThreshold", settings.depthThreshold);
+        sheet.properties.SetFloat("_NormalThreshold", settings.normalThreshold);
+        sheet.properties.SetFloat("_DepthNormalThreshold", settings.depthNormalThreshold);
+        sheet.properties.SetFloat("_DepthNormalThresholdScale", settings.depthNormalThresholdScale);
+
+        // expose camera's view space (i.e. inverse projection matrix) to Outline shader
+        Matrix4x4 clipToView = GL.GetGPUProjectionMatrix(context.camera.projectionMatrix, true).inverse;
+        sheet.properties.SetMatrix("_ClipToView", clipToView);
+
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0); 
+    }
+}
