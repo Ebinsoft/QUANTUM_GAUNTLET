f4d1d99441ebe155e7f9e24f88cd154810400c1d
Tyler Thurston <tylerthurston1@gmail.com>
2022-07-21 13:30:30 -0400

    Create HUD for tutorial player on spawn

--- Assets/Scripts/Scenes/TutorialScene/TutorialSceneManager.cs
+++ Assets/Scripts/Scenes/TutorialScene/TutorialSceneManager.cs
@@ -8,6 +8,8 @@ public class TutorialSceneManager : MonoBehaviour
     private PlayerInputManager playerInputManager;
     public GameObject stage;
     private PlayerInput playerInput;
+
+    public UnityEngine.Object playerHUDPrefab;
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,7 +21,10 @@ public class TutorialSceneManager : MonoBehaviour
         SceneSetup();
         playerInput = playerInputManager.JoinPlayer();
         var player = playerInput.GetComponent<PlayerManager>();
-        player.transform.position = stage.GetComponent<SpawnPoints>().GetSpawnPoint();
+        player.Teleport(stage.GetComponent<SpawnPoints>().GetSpawnPoint());
+        PlayerSetting ps = CreatePlayerSetting(player);
+        CreatePlayerHUD(player, ps);
+
     }
 
     private void SceneSetup()
@@ -29,10 +34,36 @@ public class TutorialSceneManager : MonoBehaviour
         GameManager.instance.versusInfo.playerList = new List<GameObject>();
     }
 
+    // hard-code a PlayerSetting for nwo for the UI
+    private PlayerSetting CreatePlayerSetting(PlayerManager player)
+    {
+
+        PlayerSetting ps = new PlayerSetting
+        {
+            playerID = player.playerID,
+            playerName = "Player " + (player.playerID + 1),
+            device = playerInput.devices[0],
+            deviceString = playerInput.devices[0].ToString(),
+            playerType = PlayerType.Human,
+            team = new Team((TeamID)player.playerID),
+            character = Character.None
+        };
+        return ps;
+    }
+
+    private void CreatePlayerHUD(PlayerManager playerManager, PlayerSetting playerSetting)
+    {
+        GameObject hudObj = (GameObject)Instantiate(playerHUDPrefab);
+        GameObject canvas = GameObject.Find("Canvas");
+        hudObj.transform.SetParent(canvas.transform);
+
+        PlayerHUD hud = hudObj.GetComponent<PlayerHUD>();
+        hud.SetPlayer(playerManager, playerSetting);
+    }
+
     public void OnPlayerJoined(PlayerInput playerInput)
     {
         int playerID = playerInput.playerIndex;
-        Debug.Log(playerInput.devices[0]);
         // add to reference of cursor objects
         GameManager.instance.versusInfo.playerList.Add(playerInput.gameObject);
     }
