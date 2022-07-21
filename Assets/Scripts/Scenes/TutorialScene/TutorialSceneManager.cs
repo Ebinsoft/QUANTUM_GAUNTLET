d1715b85452cf4d3391066140759d0713d1d5de6
Tyler Thurston <tylerthurston1@gmail.com>
2022-07-21 13:05:07 -0400

    Setup Tutorial scene spawning, ignore playerSettings since we shouldn't need it for the tutorial.

--- Assets/Scripts/Scenes/TutorialScene/TutorialSceneManager.cs
+++ Assets/Scripts/Scenes/TutorialScene/TutorialSceneManager.cs
@@ -16,14 +16,24 @@ public class TutorialSceneManager : MonoBehaviour
 
     void Start()
     {
+        SceneSetup();
         playerInput = playerInputManager.JoinPlayer();
         var player = playerInput.GetComponent<PlayerManager>();
         player.transform.position = stage.GetComponent<SpawnPoints>().GetSpawnPoint();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SceneSetup()
     {
+        var versusInfo = GameManager.instance.versusInfo;
+        versusInfo.playerList = new List<GameObject>();
+        GameManager.instance.versusInfo.playerList = new List<GameObject>();
+    }
 
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        int playerID = playerInput.playerIndex;
+        Debug.Log(playerInput.devices[0]);
+        // add to reference of cursor objects
+        GameManager.instance.versusInfo.playerList.Add(playerInput.gameObject);
     }
 }
