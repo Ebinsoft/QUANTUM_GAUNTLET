7f33140b1f39217f0a70002fa26f8b2f03d5f06b
Tyler Thurston <tylerthurston1@gmail.com>
2022-07-21 20:03:44 -0400

    Add Quests with IQuest interface so the tutorial can go through a list of various quests for the player to do.

--- Assets/TutorialDummy.cs
+++ Assets/TutorialDummy.cs
@@ -1,23 +1,138 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TutorialDummy : MonoBehaviour
 {
     public GameObject collectible;
-    public int collectiblesRemaining = 0;
+    public TextMeshProUGUI UIText;
+    private IQuest currentQuest;
+    private List<IQuest> questList = new List<IQuest>();
+    private int questIndex = 0;
     void Start()
     {
-        TutorialSceneManager.instance.SpawnRandomCollectibles(5);
+        GenerateQuests();
+        currentQuest = questList[questIndex];
+        currentQuest.OnQuestEnter();
+        UIText.SetText(currentQuest.GetQuestText());
     }
 
     void Update()
     {
+        UIText.SetText(currentQuest.GetQuestText());
+        if (currentQuest.IsComplete())
+        {
+            currentQuest.OnQuestExit();
+
+            if (questIndex == questList.Count)
+            {
+                Debug.Log("Oh shit you win");
+            }
+            currentQuest = questList[++questIndex];
+
+            currentQuest.OnQuestEnter();
+        }
     }
 
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log("Le trigger");
+        PlayerManager pm = other.attachedRigidbody.gameObject.GetComponent<PlayerManager>();
+        if (pm != null)
+        {
+            Debug.Log(pm.currentState.ToString());
+        }
+        else
+        {
+            ProjectileManager proj = other.gameObject.GetComponent<ProjectileManager>();
+            if (proj != null)
+            {
+                Debug.Log(proj.attack.attackName);
+            }
+        }
+    }
+    public void GenerateQuests()
+    {
+        questList.Add(new CollectibleQuest
+        {
+            questType = QuestType.RandomCollectibles,
+            numCollectibles = 5,
+            questText = "Hello, bitch. I am here to teach how to play this amazing game, use Left Stick to collect the collectibles"
+        });
+        Vector3 p = transform.position;
+        p.y += 2;
+        questList.Add(new CollectibleQuest
+        {
+            questType = QuestType.SpecificCollectible,
+            point = p,
+            questText = "Haha, yes, good job you can move, now you must jump with Space Bar(keyboard) or Button South(Gamepad), collect this shit on my head."
+        });
+    }
+}
+
+
+public interface IQuest
+{
+    public QuestType GetQuestType();
+    public bool IsComplete();
+    public string GetQuestText();
+    public void OnQuestEnter();
+    public void OnQuestExit();
+
+    public void OnQuestUpdate();
+
+
+}
+
+public class CollectibleQuest : IQuest
+{
+    public QuestType questType;
+    public string questText;
+    public int numCollectibles;
+    public Vector3 point;
+
+    public QuestType GetQuestType()
+    {
+        return questType;
     }
 
+    public string GetQuestText()
+    {
+        return questText + "\n Remaining: " + TutorialSceneManager.instance.collectiblesRemaining.ToString();
+    }
+    public bool IsComplete()
+    {
+        return TutorialSceneManager.instance.collectiblesRemaining == 0;
+    }
+    public void OnQuestEnter()
+    {
+        if (questType == QuestType.RandomCollectibles)
+        {
+            TutorialSceneManager.instance.SpawnRandomCollectibles(numCollectibles);
+        }
+        else
+        {
+            TutorialSceneManager.instance.SpawnCollectibleAtPoint(point);
+        }
+    }
+    public void OnQuestExit()
+    {
+
+    }
+
+    public void OnQuestUpdate()
+    {
+
+    }
+}
+
+
+public enum QuestType
+{
+    RandomCollectibles,
+    SpecificCollectible,
+    HitDummy,
+    FightEdmond,
+    Listen
 }
