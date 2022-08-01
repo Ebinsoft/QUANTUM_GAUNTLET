78a7d238c259964d35940d43ccc6b35580e3f0fb
Tyler Thurston <tylerthurston1@gmail.com>
2022-07-31 22:12:40 -0400

    Add hand cursor acceleration

--- Assets/Scripts/Scenes/CharacterSelect/HandCursor.cs
+++ Assets/Scripts/Scenes/CharacterSelect/HandCursor.cs
@@ -26,6 +26,8 @@ public class HandCursor : MonoBehaviour
     private List<GameObject> focusedElements;
     private bool isOverRoster = false;
     private bool isSummoning = false;
+    private float timeSinceAccel = 0f;
+    private float accelTime = .5f;
     private CharacterSelectManager cs;
 
 
@@ -62,6 +64,10 @@ public class HandCursor : MonoBehaviour
     private void onMove(InputAction.CallbackContext context)
     {
         isMovePressed = playerInput.actions["Move"].ReadValue<Vector2>().magnitude > 0;
+        if (!isMovePressed)
+        {
+            timeSinceAccel = 0f;
+        }
     }
 
     private void onClick(InputAction.CallbackContext context)
@@ -232,7 +238,11 @@ public class HandCursor : MonoBehaviour
 
     void updateCursor()
     {
+        timeSinceAccel += Time.deltaTime / accelTime;
+        timeSinceAccel = Mathf.Clamp(timeSinceAccel, 0f, 1f);
+        Debug.Log(timeSinceAccel);
         currentMovement = playerInput.actions["Move"].ReadValue<Vector2>();
+        currentMovement *= timeSinceAccel;
         Vector2 moveVector = new Vector2();
         moveVector.x = transform.position.x + currentMovement.x * cursorSpeed * Time.deltaTime;
         moveVector.y = transform.position.y + currentMovement.y * cursorSpeed * Time.deltaTime;
